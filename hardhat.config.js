2e4efc823456af1790d6e63e0d358d8033b18fdb
Sanjay <m.sanjay142005@gmail.com>
2025-09-20 22:15:16 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+export default {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+};
